7fb79ed2db3cbb4331ff245edb04fc4fe1bf3a31
João Mago <joao.pedro@natahouse.com>
2022-03-25 16:26:51 -0300

    test: fixing broken tests

--- test/mocks/process-error.mock.ts
+++ test/mocks/process-error.mock.ts
@@ -1,6 +1,5 @@
 import { ProcessError } from '../../lib/model/processError.entity';
 
-export const processErrorMock = new ProcessError('error');
-processErrorMock.errorType = 'error';
+export const processErrorMock = new ProcessError({error: 'error', errorType: 'error'});
 
 export const processErrorListMock = [processErrorMock];
